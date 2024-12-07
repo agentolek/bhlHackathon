1ac191ab37123fe47b8583bd4cbc9bb8b809f75e
Łukasz Rohde <74546402+luke2k04@users.noreply.github.com>
2024-12-07 23:44:11 +0100

    loading map from backend

--- frontend/frontend/ekospace_frontend/src/components/FancyMap.tsx
+++ frontend/frontend/ekospace_frontend/src/components/FancyMap.tsx
@@ -7,7 +7,7 @@ export default function FancyMap(props: fancyMapProps) {
   return (
     <embed
       type="text/html"
-      src="fancyMap.html"
+      src="http://127.0.0.1:5000/getMap"
       width={props.width}
       height={props.height}
     />

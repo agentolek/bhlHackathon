babb19f6de5f5c9c5b5cddb5c2d9818f9b29f2bb
mgrzanka <01186969@pw.edu.pl>
2024-12-08 10:04:19 +0100

    add mass

--- backend/application/routes.py
+++ backend/application/routes.py
@@ -13,7 +13,6 @@ def send_map():
 @app.route("/getSatelites")
 def send_satelites_data():
     city = request.args.get('city', default='Warsaw', type=str).strip()
-    print(city)
     try:
         satelite_data = get_satelite_data(city)
     except Exception:

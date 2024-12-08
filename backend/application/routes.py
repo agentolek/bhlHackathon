472cde5f2c9940b5a1162899c82e88dfd3a41eb5
mgrzanka <01186969@pw.edu.pl>
2024-12-08 11:26:57 +0100

    add docsstrings and changed lat and lng displaying on map

--- backend/application/routes.py
+++ backend/application/routes.py
@@ -5,7 +5,7 @@ path_to_cwd = os.getcwd()
 sys.path.append(path_to_cwd)
 
 from application import app
-from .get_satelite_data import get_satelite_data
+from .get_satelite_data import get_satelite_data, get_location
 from olek_stuff.calc_satellite_emissions import MapMaker
 
 from flask import send_from_directory, jsonify, request, abort
@@ -15,19 +15,28 @@ from flask_cors import cross_origin
 @app.route("/getMap")
 @cross_origin()
 def send_map():
+    """"
+    Route to return html file of map with mapp of distribution of gas from asteroids
+    if they would have been destroyed
+
+    This endpoint needs city param to return the map
+    """
     city = request.args.get('city', default='Warsaw', type=str).strip()
     try:
         satelite_data = get_satelite_data(city)
+        lat, lng, observer_alt = get_location(city)
     except Exception:
         abort(404, 'Wrong city')
 
     cummulated_mass = 0
-    lat = lng = 0.0
+    # lat = lng = 0.0
     if isinstance(satelite_data, list):
-        lat = satelite_data[0]["satlat"]
-        lng = satelite_data[0]["satlng"]
         for single_satelite in satelite_data:
             cummulated_mass += single_satelite["mass"]
+        #     lat += single_satelite['satlat']
+        #     lng += single_satelite["satlng"]
+        # lat /= len(satelite_data)
+        # lng /= len(satelite_data)
     else:
         cummulated_mass = satelite_data["mass"]
         lat = satelite_data["satlat"]
@@ -41,6 +50,11 @@ def send_map():
 @app.route("/getSatelites")
 @cross_origin()
 def send_satelites_data():
+    """
+    Route to return json of informations about ateroids that are above given city
+
+    This endpoint needs city param to return the map
+    """
     city = request.args.get('city', default='Warsaw', type=str).strip()
     try:
         satelite_data = get_satelite_data(city)
